d3486b6ee7414b80cb36e72d4ae7ddc075c320f7
Paramveer <paramveermarwahafc@gmail.com>
2022-04-08 01:03:35 +0530

    Added custom manager classes

--- songs/models.py
+++ songs/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models import Count
+
+
+class CustomArtistManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(album__release_date__gte="2011-09-23")
 
 
 class Artist(models.Model):
@@ -13,9 +19,15 @@ class Artist(models.Model):
     def __str__(self) -> str:
         return self.first_name
 
+    # Default Manager
+    objects = models.Manager()
+
+    # Custom Manager returns artists whose albums were released during and after '2011-09-23'
+    custom_objects = CustomArtistManager()
+
 
 # Manager subclass that returns all albums belonging to artist_id=1
-class CustomManager(models.Manager):
+class CustomAlbumManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(artist_id=1)
 
@@ -30,8 +42,11 @@ class Album(models.Model):
     release_date = models.DateField()
     stars = models.IntegerField()
 
-    objects = models.Manager()  # default manager
-    paramveer_objects = CustomManager()  # custom manager
+    # default manager
+    objects = models.Manager()
+
+    # custom manager returns albums belonging to 'artist_id=1'
+    paramveer_objects = CustomAlbumManager()
 
     def __str__(self) -> str:
         return self.name
