ce05b9cee733f2c8633934f178a828a9c1177cd3
Paramveer <paramveermarwahafc@gmail.com>
2022-04-08 00:46:09 +0530

    Customized Admin View Page

--- songs/admin.py
+++ songs/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import Album, Artist
 
 admin.site.register(Album)
-admin.site.register(Artist)
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    model = Artist
+    list_display = ("first_name", "last_name", "instrument", "full_name")
+
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+
+admin.site.register(Artist, ArtistAdmin)
